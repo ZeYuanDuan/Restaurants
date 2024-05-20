d50ef4c21d17caf069d976ff7491a97f2f6eef4c
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-05-20 13:55:56 +0800

    build: connect-flash, express-session

--- app.js
+++ app.js
@@ -1,147 +1,160 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const flash = require("connect-flash");
+const session = require("express-session");
+const app = express();
+const port = 3000;
 
-const db = require('./models')
-const Restaurant = db.Restaurant
+const db = require("./models");
+const Restaurant = db.Restaurant;
 
-const { engine } = require('express-handlebars')
-app.set('view engine', '.hbs')
-app.set('views', './views')
+const { engine } = require("express-handlebars");
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 app.engine(
-  '.hbs',
+  ".hbs",
   engine({
-    extname: '.hbs',
+    extname: ".hbs",
     helpers: {
       getImage: function (image) {
-        return image || defaultImage
+        return image || defaultImage;
       },
       getRating: function (rating) {
-        return rating || defaultRating
-      }
-    }
+        return rating || defaultRating;
+      },
+    },
   })
-)
+);
 
 const defaultImage =
-  'https://files.oaiusercontent.com/file-M3zDX7pdTXc99vuXBiaVfkmq?se=2024-04-28T05%3A53%3A26Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Daa6d215c-9c60-4252-99bb-b87ae6215305.webp&sig=g7AnWZ49hXmDcxtJCufgcWB9HCMr7omoY7EA3DKYiuA%3D'
-const defaultRating = '尚無評分'
+  "https://files.oaiusercontent.com/file-M3zDX7pdTXc99vuXBiaVfkmq?se=2024-04-28T05%3A53%3A26Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Daa6d215c-9c60-4252-99bb-b87ae6215305.webp&sig=g7AnWZ49hXmDcxtJCufgcWB9HCMr7omoY7EA3DKYiuA%3D";
+const defaultRating = "尚無評分";
 
 // =============== Middlewares ===============
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "ThisIsSecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
 // =============== Middlewares ===============
 
 // =============== Routers ===============
-app.get('/', (req, res) => {
-  res.redirect('/restaurants')
-})
+app.get("/", (req, res) => {
+  res.redirect("/restaurants");
+});
 
-app.get('/restaurants', (req, res) => {
+app.get("/restaurants", (req, res) => {
   return Restaurant.findAll({
-    raw: true
+    raw: true,
   })
-    .then((restaurants) => res.render('index', { restaurants }))
-    .catch((err) => res.status(422).json(err))
-})
+    .then((restaurants) => res.render("index", { restaurants }))
+    .catch((err) => res.status(422).json(err));
+});
 
-app.post('/restaurants', async (req, res) => {
-  const body = req.body
+app.post("/restaurants", async (req, res) => {
+  const body = req.body;
 
   return await Restaurant.create(body)
     .then(() => {
-      res.render('detail', { restaurant: body })
+      res.render("detail", { restaurant: body });
     })
-    .catch((err) => console.log(err))
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
+app.get("/restaurants/new", (req, res) => {
+  return res.render("new");
+});
 
-app.get('/restaurants/:id', (req, res) => {
-  const { id } = req.params
+app.get("/restaurants/:id", (req, res) => {
+  const { id } = req.params;
 
   return Restaurant.findByPk(id, {
-    raw: true
+    raw: true,
   })
     .then((restaurant) => {
       if (!restaurant) {
-        return res.status(404).send('尚未建立任何餐廳')
+        return res.status(404).send("尚未建立任何餐廳");
       }
-      res.render('detail', { restaurant })
+      res.render("detail", { restaurant });
     })
-    .catch((err) => res.status(422).json(err))
-})
+    .catch((err) => res.status(422).json(err));
+});
 
-app.put('/restaurants/:id', (req, res) => {
-  const { id } = req.params
+app.put("/restaurants/:id", (req, res) => {
+  const { id } = req.params;
 
   return Restaurant.update(req.body, { where: { id } })
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch((err) => {
-      res.status(500).send('更新資料庫出現錯誤')
-      console.error(err)
-    })
-})
+      res.status(500).send("更新資料庫出現錯誤");
+      console.error(err);
+    });
+});
 
-app.get('/restaurants/:id/edit', (req, res) => {
-  const { id } = req.params
+app.get("/restaurants/:id/edit", (req, res) => {
+  const { id } = req.params;
 
   return Restaurant.findByPk(id, {
-    raw: true
+    raw: true,
   })
-    .then((restaurant) => res.render('edit', { restaurant }))
+    .then((restaurant) => res.render("edit", { restaurant }))
     .catch((err) => {
-      console.error(err)
-      res.status(500).send('更新資料庫出現錯誤')
-    })
-})
+      console.error(err);
+      res.status(500).send("更新資料庫出現錯誤");
+    });
+});
 
-app.post('/restaurants/:id/delete', (req, res) => {
-  const { id } = req.params
+app.post("/restaurants/:id/delete", (req, res) => {
+  const { id } = req.params;
 
   return Restaurant.destroy({ where: { id } })
     .then(() => {
-      res.redirect('/restaurants')
+      res.redirect("/restaurants");
     })
     .catch((err) => {
-      res.status(500).send('刪除資料庫出現錯誤')
-      console.error(err)
-    })
-})
+      res.status(500).send("刪除資料庫出現錯誤");
+      console.error(err);
+    });
+});
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword?.trim()?.toLowerCase()
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword?.trim()?.toLowerCase();
 
   return Restaurant.findAll({
     where: {
       [db.Sequelize.Op.or]: [
         {
           name: {
-            [db.Sequelize.Op.like]: `%${keyword}%`
-          }
+            [db.Sequelize.Op.like]: `%${keyword}%`,
+          },
         },
         {
           category: {
-            [db.Sequelize.Op.like]: `%${keyword}%`
-          }
-        }
-      ]
+            [db.Sequelize.Op.like]: `%${keyword}%`,
+          },
+        },
+      ],
     },
-    raw: true
+    raw: true,
   })
     .then((matchedRest) => {
-      res.render('index', { restaurants: matchedRest, keyword })
+      res.render("index", { restaurants: matchedRest, keyword });
     })
-    .catch((err) => res.status(422).json(err))
-})
+    .catch((err) => res.status(422).json(err));
+});
 // =============== Routers ===============
 
 app.listen(port, () => {
-  console.log(`express server is running on http://localhost:${port}`)
-})
+  console.log(`express server is running on http://localhost:${port}`);
+});
